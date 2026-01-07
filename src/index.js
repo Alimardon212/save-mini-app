52df36d879280c0ab412f6adb8a702fa4105076d
Alimardon212 <adhamovalimardon829@gmail.com>
2026-01-07 09:15:51 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+export default {
+  async fetch(request) {
+    if (request.method !== "POST") {
+      return new Response("OK", { status: 200 });
+    }
+
+    const data = await request.json();
+
+    return new Response(
+      JSON.stringify({ status: "received", data }),
+      { headers: { "Content-Type": "application/json" } }
+    );
+  }
+};
 
